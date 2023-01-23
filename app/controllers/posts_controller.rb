df548dd81ac0ccf1e032f1b702634fca9b3b49bb
Kene <kayyungx@gmail.com>
2023-01-23 14:14:04 +0100

    Change author_id varible in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
     author_id = posted_by
-    @posts = Post.where(author_id_id:)
+    @posts = Post.where(author_id:)
   end
 
   def show
     author_id = posted_by
-    @post = Post.where(id: params[:id], author_id_id:).order(id: DESC).first
+    @post = Post.where(id: params[:id], author_id:).order(id: :DESC).first
   end
 
   def posted_by

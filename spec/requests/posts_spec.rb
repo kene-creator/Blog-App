ff1bbebcfa0ea259778d06d92a73593fd7164aff
Kene <kayyungx@gmail.com>
2023-01-24 21:27:06 +0100

    Add test for post show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "Posts", type: :request do
       text: 'Hello World!',
       comments_counter: 0,
       likes_counter: 0,
-      author_id: user.id
+      author_id_id: user.id
     )
   end
 
@@ -34,6 +34,24 @@ RSpec.describe "Posts", type: :request do
 
       it 'has response body that include the correct placeholder text' do
         expect(response.body).to include('Here is the List of post for a given user')
+      end
+    end
+  end
+
+  context 'GET #show' do
+    before(:example) { get user_post_path(user, post) }
+
+    it 'is successful' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    context 'show template' do
+      it 'is rendered' do
+        expect(response).to render_template('show')
+      end
+
+      it 'has response body that includes correct placeholder text' do
+        expect(response.body).to include('Post detials')
       end
     end
   end

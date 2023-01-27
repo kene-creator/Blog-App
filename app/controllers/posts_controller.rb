e6e5d4c1d2829211919c3351b5e04e6d02f2972a
Kene <kayyungx@gmail.com>
2023-01-27 20:20:56 +0100

    Create post and comment on behalf of current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,28 @@ class PostsController < ApplicationController
     @post = Post.where(id: params[:id], author_id:).order(id: :DESC).first
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(**post_params, author_id: current_user.id)
+
+    if @post.save
+      flash[:success] = 'Post made successfully'
+      redirect_to "/users/#{@post.author.id}/posts/#{@post.id}"
+    else
+      flash.now[:error] = 'Error: Post could not be saved'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
   def posted_by
     params[:user_id]
   end

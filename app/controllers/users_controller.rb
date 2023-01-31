91a46e1b07293d69c0b984100bee013133b4684d
Kene <kayyungx@gmail.com>
2023-01-31 16:35:11 +0100

    Removedefualt current user from application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    redirect_to '/login' unless current_user
+  end
   def index
     @users = User.all
   end
